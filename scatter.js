6f8eb2660e9bc0fe66160e49b8540a531a122adb
mcheng3 <michael.cheng65@gmail.com>
2018-03-20 07:58:10 -0400

    Add axes labels

--- scatter.js
+++ scatter.js
@@ -19,11 +19,15 @@ var moneyyears  = [
 var createplot = function(){
     var container = d3.select("svg");
     var circles = container.selectAll("circle").data(moneyyears).enter().append("circle");
+    var y = container.append("text");
+    y.text("Money earned (in hundreds)").attr("x", 15).attr("y", 600).attr("transform", "rotate(270 15,600)");
+    var x = container.append("text");
+    x.text("years in education").attr("x", 400).attr("y", 885);
     circles.attr("cy",function(d){
-	return 900-Math.floor(d[0]/100);
+	   return 900-Math.floor(d[0]/120);
     });
     circles.attr("cx",function(d){
-	return d[1]*30;
+	   return d[1]*30;
     });
     circles.attr('r',10);
 }
