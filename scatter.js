209170122abd7efdf9dbfc0e4878b4310e9fd34a
Max Korsun <mkorsun@stuy.edu>
2018-03-19 22:45:26 -0400

    edits

--- scatter.js
+++ scatter.js
@@ -3,86 +3,35 @@
 var data = JSON.parse('[{"USA":335, "RUSSIA": 122, "CHINESE": 11, "INTERNATIONAL":79, "COMMERCIAL" : 7, "IN ORBIT": 4 }]');
 
 
-var countries = ["USA","RUSSIA","CHINA","INTERNATIONAL","COMMERCIAL","IN ORBIT"];
-var people = [335,122,11,79,7,4];
+var moneyyears  = [
+    [25636,10],
+    [35256,12],
+    [38376,13],
+    [41496,14],
+    [59124,16],
+    [69732,18],
+    [89960,20],
+    [84396,22]
+    ]
+
 
 
 var createplot = function(){
     var container = d3.select("svg");
-    var circles = container.selectAll("circle").data(people).enter().append("circle");
-    circles.attr("cx",function(d){
-	if(d == 335){
-	    return 100 + "";
-	}
-	if (d== 122){
-	    return 250+"";
-	}
-	if(d==11){
-	    return 400+"";
-	}
-	if(d==79){
-	    return 550+"";
-	}
-	if(d==7){
-	    return 700+"";
-	}
-	else{
-	    return 850+"";
-	}
-    });
+    var circles = container.selectAll("circle").data(moneyyears).enter().append("circle");
     circles.attr("cy",function(d){
-	return 500 - d;
+	return 900-Math.floor(d[0]/100);
+    });
+    circles.attr("cx",function(d){
+	return d[1]*30;
     });
     circles.attr('r',10);
-    
-}
-
-var createtext = function(){
-    var container = d3.select("svg");
-    var text = container.selectAll("text").data(countries).enter().append("text");
-    text.attr("y",550);
-    text.attr("x",function(d){
-	if(d == "USA"){
-	    return 100 + "";
-	}
-	if (d== "RUSSIA"){
-	    return 200+"";
-	}
-	if(d=="CHINA"){
-	    return 350+"";
-	}
-	if(d=="INTERNATIONAL"){
-	    return 500+"";
-	}
-	if(d=="COMMERCIAL"){
-	    return 650+"";
-	}
-	else{
-	    return 800+"";
-	}
-    });
-    text.text(function(d){
-	return d;
-    });
 }
 
 
-var createnums = function(){
-    var container = d3.select("svg");
-    var text = container.selectAll("text").data(people).enter().append("text");
-    text.attr("x",50);
-    text.attr("y",function(d){
-	return 500 - d;
-    });
-    text.text(function(d){
-	return d;
-    });
-}
 
-var button = document.getElementById("button");
-button.addEventListener("click",createplot);
-button.addEventListener("click",createtext);
-button.addEventListener("click",createnums);	
+createplot();
+	
 	      
 	    
 	
